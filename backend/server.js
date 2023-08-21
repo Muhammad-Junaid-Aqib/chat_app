602ba6fc33eb3ca9b04d5196b5c3e25e69cd27a4
Muhammad Junaid Aqib <muhammad.junaid@attech-ltd.com>
2023-08-21 13:47:03 +0500

    second commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 app.get('/api/chat', (req, res) => { 
     res.send(chats)
-})
+}) 
 
 app.get('/api/chat/:id', (req, res) => { 
     const singleChat = chats.find((c) => c._id === req.params.id)
